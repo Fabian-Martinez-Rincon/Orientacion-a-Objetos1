fbb10fd4d7a3369d899640b380455879302859b0
Fabian-Martinez1 <fabianmartinezrincon.123@gmail.com>
2022-08-31 22:38:20 +0200

    Cosas

---
+++ Practica/ejercicio1-wallpost/src/main/java/ar/edu/unlp/oo1/ejercicio1/impl/WallPostImpl.java
@@ -0,0 +1,29 @@
+package ar.edu.unlp.oo1.ejercicio1.impl;
+
+import ar.edu.unlp.oo1.ejercicio1.WallPost;
+
+/**
+ * Completar esta clase de acuerdo a lo especificado en el cuadernillo
+ *
+ */
+public class WallPostImpl implements WallPost {
+
+	/**
+	 * Complete con su implementación
+	 */
+
+	
+	/*
+	 * Este mensaje se utiliza para que una instancia de Wallpost se muestre de forma adecuada
+	 */
+    @Override
+    public String toString() {
+        return "WallPost {" +
+            "text: " + getText() +
+            ", likes: '" + getLikes() + "'" +
+            ", featured: '" + isFeatured() + "'" +
+            "}";
+    }
+
+
+}
